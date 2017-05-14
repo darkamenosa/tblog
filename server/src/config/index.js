878b33d167a1e434f65008c1ef9ebfa7c948c58f
tuyenhx <hxtxmu@gmail.com>
2017-05-15 00:47:45 +0700

    Add first integration test.

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,6 +1,7 @@
 // @flow
 
 export default {
+  production: process.env.NODE_ENV === 'production',
   server: {
     port: process.env.PORT || 3000,
   },
--- server/src/index.js
+++ server/src/index.js
@@ -3,14 +3,85 @@ import express from 'express'
 import compression from 'compression'
 import path from 'path'
 
+import bodyParser from 'body-parser'
+import session from 'express-session'
+import errorhandler from 'errorhandler'
+import cors from 'cors'
+
+import logger from 'morgan'
+import methodOverride from 'method-override'
+// import mongoose from 'mongoose'
+
 import config from './config'
 
-const app = express()
-app.use(compression())
+const startServer = (cfg: Object) => {
+  const app = express()
 
-app.use(express.static(path.resolve(__dirname, 'public')))
+  // Configure express middleware
+  app.use(cors())
+  app.use(logger('dev'))
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.json())
+  app.use(methodOverride())
+  app.use(compression())
+  app.use(express.static(path.resolve(__dirname, 'public')))
+  app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }))
 
-app.listen(config.server.port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is listening on port: ${config.server.port}`)
-})
+  if (cfg.production) {
+    app.use(errorhandler())
+  }
+
+  // catch 404 and forward to error handler
+  app.use((req, res, next) => {
+    const err : Object = new Error('Not Found')
+    err.status = 404
+    next(err)
+  })
+
+  // error handlers
+
+  // development error handler
+  // will print stacktrace
+  if (!cfg.production) {
+    app.use((err, req, res) => {
+      // eslint-disable-next-line no-console
+      console.log(err.stack)
+
+      res.status(err.status || 500)
+
+      res.json({
+        errors: {
+          message: err.message,
+          error: err,
+        },
+      })
+    })
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
+  app.use((err, req, res) => {
+    res.status(err.status || 500)
+    res.json({
+      errors: {
+        message: err.message,
+        error: {},
+      },
+    })
+  })
+
+
+  return new Promise((resolve) => {
+    const server = app.listen(cfg.server.port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Server is listening on port: ${cfg.server.port}`)
+      resolve(server)
+    })
+  })
+}
+
+if (require.main === module) {
+  startServer(config)
+}
+
+export default startServer
