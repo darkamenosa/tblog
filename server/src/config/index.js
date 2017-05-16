f6d534d02fc0e6820d6a78f9683014d9eb985c0d
tuyenhx <hxtxmu@gmail.com>
2017-05-16 08:48:20 +0700

    Change DB config.

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -3,7 +3,7 @@
 export default {
   production: process.env.NODE_ENV === 'production',
   db: {
-    url: process.env.MONGOHQ_URL || 'mongodb://localhost/tblog',
+    url: process.env.MONGODB_URI || 'mongodb://localhost/tblog',
   },
   server: {
     port: process.env.PORT || 3000,
