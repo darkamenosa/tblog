b6303d55391e832439b184255017d05243b8cdd7
tuyenhx <hxtxmu@gmail.com>
2017-05-22 00:39:42 +0700

    Add static layout for search page.

--- webclient/app/containers/SearchPage/index.js
+++ webclient/app/containers/SearchPage/index.js
@@ -6,25 +6,66 @@
 
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import Helmet from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
+import styled from 'styled-components';
+import { FormControl, FormGroup } from 'react-bootstrap';
 import AppWrapper from 'components/AppWrapper';
+import ArticleList from 'components/ArticleList';
 import makeSelectSearchPage from './selectors';
-import messages from './messages';
+
+const ContentWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+
+const SearchInput = styled(FormControl)`
+  border: 0;
+  background-color: rgb(250, 250, 250);
+  border-radius: 0;
+  box-shadow: none;
+
+  border-bottom: 1px solid rgba(0,0,0,.15);
+`;
+
+const fakeArticles = [
+  {},
+  {},
+];
 
 export class SearchPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <AppWrapper>
-        <div>
-          <Helmet
-            title="SearchPage"
-            meta={[
-              { name: 'description', content: 'Description of SearchPage' },
-            ]}
-          />
-          <FormattedMessage {...messages.header} />
+      <AppWrapper
+        title="SearchPage"
+        meta={[
+          { name: 'description', content: 'Description of SearchPage' },
+        ]}
+      >
+        <div className="row">
+          <ContentWrapper>
+            <div className="col-xs-12 col-sm-6">
+              <form>
+                <FormGroup>
+                  <SearchInput
+                    bsSize="large"
+                    type="text"
+                    placeholder="Input text to search"
+                  />
+                </FormGroup>
+              </form>
+            </div>
+          </ContentWrapper>
+        </div>
+        <div className="row">
+          <ContentWrapper>
+            <div className="col-xs-12 col-sm-6">
+              <b>Search results</b>
+              <ArticleList articles={fakeArticles} />
+            </div>
+          </ContentWrapper>
         </div>
       </AppWrapper>
     );
